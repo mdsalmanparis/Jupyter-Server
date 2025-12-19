1eda1fc68914d5fcb2390a9e27ae4e3ea7e7dad9
Mohamed Salman Paris M <salmanparis.dev@gmail.com>
2025-12-19 06:45:53 +0000

    cors commit

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-jupyter lab --ip=0.0.0.0 --port=$PORT --no-browser --NotebookApp.token='' --NotebookApp.password=''
+jupyter lab --ip=0.0.0.0 --port=$PORT --no-browser \
+  --NotebookApp.allow_origin='*' \
+  --NotebookApp.allow_origin_pat='*' \
+  --NotebookApp.disable_check_xsrf=True \
+  --NotebookApp.token='' \
+  --NotebookApp.password=''
