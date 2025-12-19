76611c29e03de5fec9c95b6fccd86f071e0626db
Mohamed Salman Paris M <salmanparis.dev@gmail.com>
2025-12-19 07:06:21 +0000

    change 2

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 jupyter lab --ip=0.0.0.0 --port=$PORT --no-browser \
-  --ServerApp.allow_origin='*' \
-  --ServerApp.allow_origin_pat='.*' \
+  --ServerApp.allow_origin_pat=.* \
   --ServerApp.disable_check_xsrf=True \
   --ServerApp.token='' \
   --ServerApp.password=''
